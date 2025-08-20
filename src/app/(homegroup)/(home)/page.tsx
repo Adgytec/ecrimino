88c17bb26b14c70438aab4612e21c2a88590b1b1
rohan <rohanverma031@gmail.com>
2025-08-20 17:53:19 +0530

    update: updated aculite position in home page

--- src/app/(homegroup)/(home)/page.tsx
+++ src/app/(homegroup)/(home)/page.tsx
@@ -5,32 +5,32 @@ import Events from "./components/events/Events";
 
 import { banner, academics, ccps } from "@/data/home/data";
 
-export const revalidate = 60 * 60;
+export const revalidate = 3600;
 
 export default async function Home() {
-	const url = `${process.env.NEXT_PUBLIC_API}/services/blogs?limit=5`;
-	const blogs: Blog[] | null = await fetch(url, {
-		method: "GET",
-		headers: {
-			Authorization: `Bearer ${process.env.NEXT_PUBLIC_TOKEN}`,
-		},
-	})
-		.then((res) => res.json())
-		.then((res) => {
-			if (res.error) throw new Error(res.message);
-			return res.data.blogs;
-		})
-		.catch((err) => {
-			console.error(err.message);
-			return null;
-		});
+  const url = `${process.env.NEXT_PUBLIC_API}/services/blogs?limit=5`;
+  const blogs: Blog[] | null = await fetch(url, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_TOKEN}`,
+    },
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      if (res.error) throw new Error(res.message);
+      return res.data.blogs;
+    })
+    .catch((err) => {
+      console.error(err.message);
+      return null;
+    });
 
-	return (
-		<>
-			<Banner data={banner} />
-			<Cards data={academics} />
-			<Cards data={ccps} />
-			{blogs && <Events blogs={blogs} />}
-		</>
-	);
+  return (
+    <>
+      <Banner data={banner} />
+      {blogs && <Events blogs={blogs} />}
+      <Cards data={academics} />
+      <Cards data={ccps} />
+    </>
+  );
 }
